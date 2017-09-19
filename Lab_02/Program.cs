3085437f035e7ed77d60e6be4224fc0ae6614aff
Josh-Romito <jromito71@gmail.com>
2017-09-19 19:48:19 -0400

    Add project files.

---
+++ Lab_02/Program.cs
@@ -0,0 +1,49 @@
+﻿/********************
+ * Joshua Romito   
+ * Septemeber 19th 2017
+ * Lab_02
+ *********************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Part 1.");
+            Console.WriteLine("-----------------------------");
+            //instantiating first employee - with no next in command
+            Worker johnny = new Worker("Johnny", 1000000000, "OWNER", null);
+            
+            //authorizing amounts
+            johnny.Authorize(1000);
+            johnny.Authorize(1000000);
+
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine("Part 2.");
+            Console.WriteLine("-----------------------------");
+           
+            //instantiating multiple employee's
+            Worker CEO = new Worker("Josh Romito", 1000000000, "CEO", null);        
+            Worker CFO = new Worker("Juan", 1000000, "CFO", CEO);
+            Worker manager = new Worker("Michael", 100000, "Manager", CFO);
+            Worker supervisor = new Worker("Jim", 10000, "Supervisor", manager);
+            Worker employee = new Worker("Mike", 1000, "Employee", supervisor);
+
+            //calling authorize on base employee
+            employee.Authorize(100);
+            employee.Authorize(5000);
+            employee.Authorize(75000);
+            employee.Authorize(250000);
+            employee.Authorize(651243);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_02/Worker.cs
@@ -0,0 +1,49 @@
+﻿/********************
+ * Joshua Romito   
+ * Septemeber 19th 2017
+ * Lab_02
+ *********************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02
+{
+    class Worker
+    {
+        //declaring fields
+        private double _upperLimit;
+        private string _classification;
+        private string _name;
+        private Worker _nextInCommand;
+        
+        public void Authorize(double amount)
+        {
+            //if our amount is less than or equal to the upper limit
+            if (amount <= _upperLimit)
+            {
+                Console.WriteLine("The amount " + amount + " is okayed by " + _name + " (" + _classification + ")");
+            }
+            else   // if amount is greater than 
+            {
+                //if we have a next in command
+                if (_nextInCommand != null)
+                {
+                    //then call authorize
+                    _nextInCommand.Authorize(amount);
+                }
+            }
+        }
+
+        //Constructor - assigning fields based on parameters
+        public Worker(string name, double upperLimit, string classification, Worker nextInCommand)
+        {
+            _name = name;
+            _upperLimit = upperLimit;
+            _classification = classification;
+            _nextInCommand = nextInCommand;
+        }
+    }
+}
